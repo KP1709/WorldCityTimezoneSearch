225cc11831e3aba8b36dc3163448630c5c1c1f37
Kareena Patel <kareenapat17@gmail.com>
2025-07-27 11:04:26 +0100

    Removed commented code

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -19,7 +19,6 @@ function TimeCardInfo({ chosenCity, markerTimeData }: { chosenCity: CitiesType, 
                     <path d="M232,128A104,104,0,1,1,128,24,104.13,104.13,0,0,1,232,128Z"></path>
                 </svg>
             </span>
-            {/* <span id='date'> {DateTime.now().setZone(markerTimeData?.zoneName).toFormat('DDD') ?? null}</span> */}
             <span id='date'><TimeDate timezone={markerTimeData?.zoneName} time={false} /> </span>
             <span id='timezone-id'>{DateTime.now().setZone(markerTimeData?.zoneName).toFormat('ZZZZZ') ?? null}</span>
             <span id='timezone-name'>{markerTimeData?.zoneName}</span>
