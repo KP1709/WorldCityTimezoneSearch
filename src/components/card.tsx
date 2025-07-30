55cd3f675f81eda2db3f1275ba85d188d3a4f55b
Kareena Patel <kareenapat17@gmail.com>
2025-07-30 20:48:11 +0100

    Add collapsable card for phone view only

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -6,14 +6,52 @@ import '../components/styles/cardStyles.css'
 import useBreakpoint from "../hooks/useBreakpoint";
 import FlagImage from "./flagImage";
 import { getFlagImage } from "../hooks/getFlagImage";
+import { useState } from "react";
+
+function ToggleCardButton({ isExpanded, setIsExpanded }: { isExpanded: boolean, setIsExpanded: (value: boolean) => void }) {
+    return (
+        <button id='card-toggle' onClick={() => setIsExpanded(!isExpanded)}>
+            {isExpanded ? <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256">
+                <path d="M213.66,101.66l-80,80a8,8,0,0,1-11.32,0l-80-80A8,8,0,0,1,53.66,90.34L128,164.69l74.34-74.35a8,8,0,0,1,11.32,11.32Z"></path>
+            </svg>
+                : <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256">
+                    <path d="M213.66,165.66a8,8,0,0,1-11.32,0L128,91.31,53.66,165.66a8,8,0,0,1-11.32-11.32l80-80a8,8,0,0,1,11.32,0l80,80A8,8,0,0,1,213.66,165.66Z"></path>
+                </svg>}
+        </button>
+    )
+}
 
 function TimeCardInfo({ chosenCity, markerTimeData }: { chosenCity: CitiesType, markerTimeData: TimeZoneType | undefined }) {
     if (!markerTimeData) return null
     const { flags } = getFlagImage({ markerTimeData });
     const { mainFlag, secondaryFlag } = flags
+    const currentBreakpoint = useBreakpoint()
+
+    const [isSmallScreen] = useState(() => {
+        if (currentBreakpoint < 500) return true
+        else return false
+    })
+    const [isExpanded, setIsExpanded] = useState(false)
+
+    if (currentBreakpoint < 500 && !isExpanded) {
+        return (
+            <div id='main-card' className='small' style={{ height: '100px' }}>
+                {isSmallScreen && <ToggleCardButton isExpanded={isExpanded} setIsExpanded={setIsExpanded} />}
+                <span id="clock"><TimeDate timezone={markerTimeData?.zoneName} /> {markerTimeData?.abbreviation}
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" aria-hidden="true">
+                        <path d="M232,128A104,104,0,1,1,128,24,104.13,104.13,0,0,1,232,128Z"></path>
+                    </svg>
+                </span>
+                <span id='date-small'><TimeDate timezone={markerTimeData?.zoneName} time={false} /> </span>
+                <span id='location-small'>{chosenCity?.city_ascii}, {markerTimeData?.regionName}, {markerTimeData?.countryName}</span>
+            </div>
+        )
+    }
 
     return (
-        <div id='main-card'>
+        <div id='main-card' className='large' style={{ height: '275px' }}>
+            {isExpanded && <ToggleCardButton isExpanded={isExpanded} setIsExpanded={setIsExpanded} />}
+
             <span id="clock"><TimeDate timezone={markerTimeData?.zoneName} /> {markerTimeData?.abbreviation}
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" aria-hidden="true">
                     <path d="M232,128A104,104,0,1,1,128,24,104.13,104.13,0,0,1,232,128Z"></path>
